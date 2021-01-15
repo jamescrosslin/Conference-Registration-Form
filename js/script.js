33dd850cf9bd6b764a11f9db1662c188610c6239
James Crosslin <jamesmscrosslin@gmail.com>
2021-01-14 21:10:18 -0800

    added comments

--- js/script.js
+++ js/script.js
@@ -4,16 +4,25 @@ const otherJobRole = document.getElementById("other-job-role");
 const shirtDesign = document.getElementById("design");
 const shirtColor = document.getElementById("color");
 const activities = document.getElementById("activities");
+const ccField = document.getElementById("credit-card");
 
 /**
- * this IIFE provides the beginning page state
+ * @description IIFE alters the beginning page state
  */
 (() => {
   nameInput.focus();
   toggleDisplay(otherJobRole, false);
   shirtColor.setAttribute("disabled", "true");
+  toggleDisplay(ccField.nextElementSibling, false);
+  toggleDisplay(ccField.nextElementSibling.nextElementSibling, false);
 })();
 
+/**
+ * @function toggleDisplay
+ * @param {element} element
+ * @param {boolean} condition
+ * @description shows or hides an element based on a condition
+ */
 function toggleDisplay(element, condition) {
   element.style.display = condition ? "inherit" : "none";
 }
@@ -22,6 +31,11 @@ title.addEventListener("change", () => {
   toggleDisplay(otherJobRole, title.value === "other");
 });
 
+/**
+ * @listens shirtDesign
+ * @description on design selection, displays only applicable
+ * color selections and auto-selects first available
+ */
 shirtDesign.addEventListener("change", (e) => {
   shirtColor.removeAttribute("disabled");
   [...shirtColor.children].filter((option) => {
@@ -32,6 +46,11 @@ shirtDesign.addEventListener("change", (e) => {
   })[0].selected = true;
 });
 
+/**
+ * @listens activities the "Register for Activities" container
+ * @description on activity checkbox change, adds or subracts the
+ * price of activity from the total
+ */
 activities.addEventListener("change", (e) => {
   const cost = +e.target.getAttribute("data-cost");
   let activitiesCost = document.getElementById("activities-cost");
